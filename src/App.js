abce7712acb885ab0037b6e79690d5b81e7c1201
sangbeom <bumeeya2@naver.com>
2021-04-25 16:49:47 +0900

    20210423 second study

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
+function Food (props){
+  return <h3>I Love {props.fav}</h3>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!</h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgipopsal" />
+      <Food fav="chukumi" />
     </div>
   );
 }
